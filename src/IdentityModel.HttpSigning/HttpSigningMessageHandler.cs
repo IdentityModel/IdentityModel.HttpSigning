69668d6254a1bc1b182ebdec7730df6c8d54694b
Brock Allen <brockallen@gmail.com>
2016-02-25 21:45:11 -0500

    add signing message handler

---
+++ src/IdentityModel.HttpSigning/HttpSigningMessageHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityModel.HttpSigning
+{
+    public class HttpSigningMessageHandler : DelegatingHandler
+    {
+        private readonly Signature _signature;
+        private readonly SigningOptions _options;
+
+        public HttpSigningMessageHandler(Signature signature, SigningOptions options, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            if (signature == null) throw new ArgumentNullException("signature");
+
+            _signature = signature;
+            _options = options ?? new SigningOptions();
+        }
+
+        public HttpSigningMessageHandler(Signature signature, HttpMessageHandler innerHandler)
+            : this(signature, null, innerHandler)
+        {
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await ProcessSignatureAsync(request);
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        public async Task ProcessSignatureAsync(HttpRequestMessage request)
+        {
+            var parameters = await _options.CreateEncodingParametersAsync(request);
+            if (parameters != null)
+            {
+                var token = _signature.Sign(parameters);
+                request.AddPopToken(token);
+            }
+        }
+    }
+}
---
+++ src/IdentityModel.HttpSigning/SigningOptions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModel.HttpSigning
+{
+    public class SigningOptions
+    {
+        public bool SignHttpMethod { get; set; }
+        public bool SignHost { get; set; }
+        public bool SignUrlPath { get; set; }
+        public bool SignAllQueryParameters { get; set; }
+        public IEnumerable<string> QueryParametersToSign { get; set; }
+        public IEnumerable<string> RequestHeadersToSign { get; set; }
+        public bool SignBody { get; set; }
+
+        public async Task<EncodingParameters> CreateEncodingParametersAsync(HttpRequestMessage request)
+        {
+            if (request == null) throw new ArgumentNullException("request");
+
+            var token = request.GetAccessToken();
+            if (token == null) return null;
+
+            var parameters = new EncodingParameters(token);
+
+            if (SignHttpMethod)
+            {
+                parameters.HttpMethod = request.Method;
+            }
+
+            if (SignHost)
+            {
+                parameters.Host = request.RequestUri.Host;
+            }
+
+            if (SignUrlPath)
+            {
+                parameters.UrlPath = request.RequestUri.AbsolutePath;
+            }
+
+            var query = request.RequestUri.ParseQueryString();
+
+            var queryParamsToSign = QueryParametersToSign;
+            if (SignAllQueryParameters)
+            {
+                queryParamsToSign = query.Keys.Cast<string>();
+            }
+
+            if (queryParamsToSign != null && queryParamsToSign.Any())
+            {
+                foreach (var key in queryParamsToSign)
+                {
+                    var value = query[key];
+                    if (value != null)
+                    {
+                        parameters.QueryParameters.Add(new KeyValuePair<string, string>(key, value));
+                    }
+                }
+            }
+
+            if (RequestHeadersToSign != null && RequestHeadersToSign.Any())
+            {
+                foreach (var key in RequestHeadersToSign)
+                {
+                    IEnumerable<string> values;
+                    if (request.Headers.TryGetValues(key, out values))
+                    {
+                        foreach (var value in values)
+                        {
+                            parameters.RequestHeaders.Add(new KeyValuePair<string, string>(key, value));
+                        }
+                    }
+                }
+            }
+
+            if (SignBody)
+            {
+                parameters.Body = await request.ReadBodyAsync();
+            }
+
+            return parameters;
+        }
+    }
+}
