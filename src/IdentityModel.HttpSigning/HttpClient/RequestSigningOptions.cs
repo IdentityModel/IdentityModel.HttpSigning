0028919733c5dfd577bbdf27c04223d5554a6c10
Brock Allen <brockallen@gmail.com>
2016-04-01 09:41:30 -0400

    cleanup nuspec

--- src/IdentityModel.HttpSigning/HttpClient/RequestSigningOptions.cs
+++ src/IdentityModel.HttpSigning/HttpClient/RequestSigningOptions.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace IdentityModel.HttpSigning
